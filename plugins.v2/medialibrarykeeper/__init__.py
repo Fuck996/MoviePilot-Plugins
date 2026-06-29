092fcebd5ee483b8078cc867c76d0aebea6b8ab4
Fuck996 <49037819+Fuck996@users.noreply.github.com>
2026-06-29 12:03:09 +0800

    Refresh medialibrarykeeper package release

--- plugins.v2/medialibrarykeeper/__init__.py
+++ plugins.v2/medialibrarykeeper/__init__.py
@@ -35,7 +35,7 @@ class MediaLibraryKeeper(_PluginBase):
     plugin_name = "媒体库管家"
     plugin_desc = "管理 Emby 媒体库观看进度、空间风险和清理计划。"
     plugin_icon = "emby.png"
-    plugin_version = "0.2.2"
+    plugin_version = "0.2.3"
     plugin_author = "fuck996"
     author_url = "https://github.com/Fuck996"
     plugin_config_prefix = "medialibrarykeeper_"
